4d8b71e4de8a822fa70d37ae20a4eaffe8cfa9da
Rohan Thacker <thackerrohan4@gmail.com>
2023-05-19 16:20:40 +0530

    Updated: Rolled back Line-Clamp Value

--- src/components/app_list.tsx
+++ src/components/app_list.tsx
@@ -9,7 +9,7 @@ export function AppList(props) {
                 <Link href={`/dapp?id=${app.dappId}`}>
                     <Image src={app.images.logo} width={64} height={64} className="rounded-lg" alt="" />
                     <p className="text-[24px] leading-[32px] font-[500] my-4">{app.name}</p>
-                    <Text classNames='text-[16px] leading-[21px] text-[#87868C] font-[400] line-clamp-10' maxLines={3}>{app.description}</Text>
+                    <Text classNames='text-[16px] leading-[21px] text-[#87868C] font-[400] line-clamp-3' maxLines={3}>{app.description}</Text>
                 </Link>
             </Card>) : <p className="text-xl">Oh No! We didnt find any dApps</p> }
         </div>
