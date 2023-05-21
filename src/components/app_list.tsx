44a7119a6e9e0a3d06f90d1eb70b1c3ab0bcca83
Abhimanyu121 <abhimanyushekhawat17.as@gmail.com>
2023-05-21 12:08:05 +0530

    Some minor ui fixes

--- src/components/app_list.tsx
+++ src/components/app_list.tsx
@@ -1,17 +1,17 @@
-import {Dapp} from "../features/dapp/models/dapp";
-import {Card, RImage as Image, Text} from "./index";
+import { Dapp } from "../features/dapp/models/dapp";
+import { Card, RImage as Image, Text } from "./index";
 import Link from "next/link";
 
 export function AppList(props) {
     return (
         <div className="grid gap-8 grid-cols-1 md:grid-cols-2 3xl:grid-cols-3">
-            {props.data.length ? props.data.map((app: Dapp) => <Card>
+            {props.data.length ? props.data.map((app: Dapp) => <Card key={app.dappId}>
                 <Link href={`/dapp?id=${app.dappId}`}>
                     <Image src={app.images.logo} width={64} height={64} className="rounded-lg" alt="" />
                     <p className="text-[24px] leading-[32px] font-[500] my-4">{app.name}</p>
                     <Text classNames='text-[16px] leading-[21px] text-[#87868C] font-[400] line-clamp-3' maxLines={3}>{app.description}</Text>
                 </Link>
-            </Card>) : <p className="text-xl">Oh No! We didnt find any dApps</p> }
+            </Card>) : <p className="text-xl">Oh No! We didnt find any dApps</p>}
         </div>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,64 +3,64 @@ import { AppList, Card, Hero, PageLayout, } from '../components';
 import { useGetInfiniteDappListQuery } from "../features/dapp/dapp_api";
 import { AppStrings } from "./constants";
 import Dapp from "./dapp";
-import {useSelector} from "react-redux";
-import {getApp} from "../features/app/app_slice";
+import { useSelector } from "react-redux";
+import { getApp } from "../features/app/app_slice";
 
 const Index = (props) => {
-        const app = useSelector(getApp);
-        const limit = 8;
-        const [page, setPage] = useState<number>(1);
-        const [items, setItems] = useState<Array<typeof Dapp>>([]);
-        const {
-            data,
-            isFetching,
-            isLoading,
-        } = useGetInfiniteDappListQuery({
-            page:page,
-            limit:limit,
-            chainId: app.chainId,
-        },{ 
-            refetchOnMountOrArgChange:true,
-        });
-       
-        // since now data is being merge in RTK itself
-        useEffect(() => {
-            if (data) {
-                setItems([ ...data?.response])
-            }
-        }, [data]);
+    const app = useSelector(getApp);
+    const limit = 8;
+    const [page, setPage] = useState<number>(1);
+    const [items, setItems] = useState<Array<typeof Dapp>>([]);
+    const {
+        data,
+        isFetching,
+        isLoading,
+    } = useGetInfiniteDappListQuery({
+        page: page,
+        limit: limit,
+        chainId: app.chainId,
+    }, {
+        refetchOnMountOrArgChange: true,
+    });
 
-        // const observerTarget = useRef(null);
-
-        const buildLoadingItems = () => {
-            const _items : any[] =  [];
-            for (let i = 0; i < (items.length || 10); i++) {
-                _items.push(<div key={i} className="shimmer w-full h-[160px] rounded-lg" />)
-            }
-            return _items;
+    // since now data is being merge in RTK itself
+    useEffect(() => {
+        if (data) {
+            setItems([...data?.response])
         }
+    }, [data]);
 
-        // const opt = {
-        //     threshold: 1,
-        // };
+    // const observerTarget = useRef(null);
 
-        // this one doesn't need to be refrished to listen to scroll events
-        useEffect(() => {
-            const onScroll = () => {
-              const scrolledToBottom =
-                window.innerHeight + window.scrollY + window.innerHeight/3 >= document.body.offsetHeight;
-              if   (scrolledToBottom && !isFetching &&( page <( data?.pageCount || 0)) ) {
+    const buildLoadingItems = () => {
+        const _items: any[] = [];
+        for (let i = 0; i < (items.length || 10); i++) {
+            _items.push(<div key={i} className="shimmer w-full h-[160px] rounded-lg" />)
+        }
+        return _items;
+    }
+
+    // const opt = {
+    //     threshold: 1,
+    // };
+
+    // this one doesn't need to be refrished to listen to scroll events
+    useEffect(() => {
+        const onScroll = () => {
+            const scrolledToBottom =
+                window.innerHeight + window.scrollY + window.innerHeight / 3 >= document.body.offsetHeight;
+            if (scrolledToBottom && !isFetching && (page < (data?.pageCount || 1))) {
                 console.log("Fetching more data...");
                 setPage(page + 1);
-              }
-            };
-        
-            document.addEventListener("scroll", onScroll);
-        
-            return function () {
-              document.removeEventListener("scroll", onScroll);
-            };
-          }, [page, isFetching]);
+            }
+        };
+
+        document.addEventListener("scroll", onScroll);
+
+        return function () {
+            document.removeEventListener("scroll", onScroll);
+        };
+    }, [page, isFetching]);
 
     //     useEffect(() => {
     //         console.log('Using Effect');
@@ -82,20 +82,21 @@ const Index = (props) => {
     //         };
     // }, [observerTarget.current,  page,isFetching]);
 
-        let child;
-        if ((isLoading || isFetching) && items.length === 0) return <PageLayout>
-            <div>
-                <div className="bg-border-color w-[240px] h-[32px] my-4"/>
-                <div className="grid gap-8 grid-cols-1 md:grid-cols-2 3xl:grid-cols-3">
-                    {buildLoadingItems()}
-                </div>
+    let child;
+    console.log(items);
+    if ((isLoading || isFetching) && ((items.length === 0) || ((items[0] as any).chains as Array<number>).indexOf(app.chainId) === -1)) return <PageLayout>
+        <div>
+            <div className="bg-border-color w-[240px] h-[32px] my-4" />
+            <div className="grid gap-8 grid-cols-1 md:grid-cols-2 3xl:grid-cols-3">
+                {buildLoadingItems()}
             </div>
-        </PageLayout>
+        </div>
+    </PageLayout>
 
-        child = (<>
-                <AppList data={items}/>
-            </>
-        );
+    child = (<>
+        <AppList data={items} />
+    </>
+    );
 
     return (
         <>
@@ -109,11 +110,18 @@ const Index = (props) => {
             </div>
             <PageLayout>
                 <h1 className="text-4xl mb-8 capitalize">{AppStrings.allDapps}</h1>
-                <div className="h-[54px] w-full"/>
+                <div className="h-[54px] w-full" />
                 {child}
                 {/* <div ref={observerTarget} /> */}
                 {/* TODO add loader here */}
-                <p>Loading more</p>
+                {(isLoading || isFetching) ? <div>
+                    <div className="h-[35px] w-full" />
+
+                    <div className="grid gap-8 grid-cols-1 md:grid-cols-2 3xl:grid-cols-3">
+                        {buildLoadingItems()}
+                    </div>
+                </div> : <div />}
+
             </PageLayout>
         </>
     )
