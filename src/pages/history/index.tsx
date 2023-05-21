e2e744f9a7270fa9e5f3fe5952fdab17641d8bcf
Abhimanyu121 <abhimanyushekhawat17.as@gmail.com>
2023-05-21 13:41:45 +0530

    Fixed browsing history

--- src/pages/history/index.tsx
+++ src/pages/history/index.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { getApp } from "../../features/app/app_slice";
 import { AppStrings } from "../constants";
+import { Dapp } from "../../features/dapp/models/dapp";
 
 export default function HistoryPage(props) {
     const app = useSelector(getApp);
@@ -12,13 +13,15 @@ export default function HistoryPage(props) {
         if (text) {
             setHistory(JSON.parse(text));
         }
+        console.log(history);
 
     }, [])
     return (
         <PageLayout>
             <h1 className="text-4xl mb-8 capitalize">{AppStrings.browsingHistory}</h1>
             {props.subtitle && <h2 className="text-[20px] leading-[28px]  mb-8 capitalize">{'props.subtitle'}</h2>}
-            <AppList data={Object.values(history)} />
+            <AppList data={(Object.values(history) as Array<Dapp>).filter((dapp) => ((dapp.chains as Array<number>).indexOf(app.chainId) !== -1)).reverse()} />
+
         </PageLayout>
-        )
+    )
 }
