b3d81a4a85f4e62bb698a759b57837107a5f8c88
Abhimanyu121 <abhimanyushekhawat17.as@gmail.com>
2023-05-21 14:49:45 +0530

    disabled eslint

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: 'build',
-
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   i18n: {
     locales: ['en'],
     defaultLocale: 'en'
