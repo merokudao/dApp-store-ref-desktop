7b5ad396171dc4612bc74644f55bfd051c72f8b0
Abhimanyu121 <abhimanyushekhawat17.as@gmail.com>
2023-05-28 16:08:44 +0530

    seggregated others

--- src/features/categories/index.ts
+++ src/features/categories/index.ts
@@ -1,212 +1,212 @@
-let merokuJsonString = `{
-    "message": "success",
-    "data": [
-      {
-        "category": "books",
-        "subCategory": [
-          "ebooks",
-          "audiobooks",
-          "document-readers"
-        ]
-      },
-      {
-        "category": "business",
-        "subCategory": [
-          "communication",
-          "project-management",
-          "human-resources",
-          "decentralized-business-tools"
-        ]
-      },
-      {
-        "category": "developer tools",
-        "subCategory": [
-          "discovery tool",
-          "developer infra"
-        ]
-      },
-      {
-        "category": "education",
-        "subCategory": [
-          "learning tools",
-          "reference",
-          "language-learning",
-          "stem"
-        ]
-      },
-      {
-        "category": "entertainment",
-        "subCategory": [
-          "music",
-          "video",
-          "video-streaming",
-          "music-streaming",
-          "live-events",
-          "nft-marketplaces"
-        ]
-      },
-      {
-        "category": "defi",
-        "subCategory": [
-          "banking",
-          "personal-finance",
-          "exchanges",
-          "insurance",
-          "on-ramping",
-          "off-ramping",
-          "payments",
-          "finance",
-          "airdrop tool",
-          "others",
-          "tooling",
-          "prediction markets",
-          "lending-and-borrowing",
-          "infrastructure"
-        ]
-      },
-      {
-        "category": "food and drink",
-        "subCategory": [
-          "cooking",
-          "recipes",
-          "restaurant-finding"
-        ]
-      },
-      {
-        "category": "games",
-        "subCategory": [
-          "action",
-          "adventure",
-          "puzzle",
-          "role-playing",
-          "strategy",
-          "racing",
-          "board ",
-          "simulation",
-          "word",
-          "metaverse"
-        ]
-      },
-      {
-        "category": "health and fitness",
-        "subCategory": [
-          "workout-apps",
-          "meditation",
-          "nutrition",
-          "sleep-trackers",
-          "medical"
-        ]
-      },
-      {
-        "category": "lifestyle",
-        "subCategory": [
-          "home-automation",
-          "fashion",
-          "dating"
-        ]
-      },
-      {
-        "category": "kids",
-        "subCategory": []
-      },
-      {
-        "category": "news",
-        "subCategory": [
-          "sports-news",
-          "magazines",
-          "decentralized-news-platforms",
-          "newspapers",
-          "live-news"
-        ]
-      },
-      {
-        "category": "photography",
-        "subCategory": [
-          "photo-editing",
-          "camera-apps",
-          "photo-sharing"
-        ]
-      },
-      {
-        "category": "productivity",
-        "subCategory": [
-          "note-taking",
-          "task-management",
-          "time-management",
-          "calendar",
-          "decentralized-collaboration-tools",
-          "graphics and design"
-        ]
-      },
-      {
-        "category": "shopping",
-        "subCategory": [
-          "ecommerce",
-          "nft-marketplaces",
-          "price aggregator"
-        ]
-      },
-      {
-        "category": "social networking",
-        "subCategory": [
-          "decentralized-social-networks",
-          "messaging"
-        ]
-      },
-      {
-        "category": "sports",
-        "subCategory": [
-          "team-management",
-          "live-scores"
-        ]
-      },
-      {
-        "category": "travel",
-        "subCategory": [
-          "navigation",
-          "accommodation-booking",
-          "transportation",
-          "trip-planning"
-        ]
-      },
-      {
-        "category": "utilities",
-        "subCategory": [
-          "file-management",
-          "browsers",
-          "security-and-privacy",
-          "wallets",
-          "weather"
-        ]
-      },
-      {
-        "category": "nft",
-        "subCategory": [
-          "art",
-          "pfps",
-          "domain names"
-        ]
-      },
-      {
-        "category": "gambling",
-        "subCategory": []
-      },
-      {
-        "category": "social",
-        "subCategory": [
-          "social media"
-        ]
-      },
-      {
-        "category": "personalization",
-        "subCategory": [
-          "themes",
-          "wallpapers",
-          "customization-tools"
-        ]
-      }
-    ]
-  }`;
+// let merokuJsonString = `{
+//     "message": "success",
+//     "data": [
+//       {
+//         "category": "books",
+//         "subCategory": [
+//           "ebooks",
+//           "audiobooks",
+//           "document-readers"
+//         ]
+//       },
+//       {
+//         "category": "business",
+//         "subCategory": [
+//           "communication",
+//           "project-management",
+//           "human-resources",
+//           "decentralized-business-tools"
+//         ]
+//       },
+//       {
+//         "category": "developer tools",
+//         "subCategory": [
+//           "discovery tool",
+//           "developer infra"
+//         ]
+//       },
+//       {
+//         "category": "education",
+//         "subCategory": [
+//           "learning tools",
+//           "reference",
+//           "language-learning",
+//           "stem"
+//         ]
+//       },
+//       {
+//         "category": "entertainment",
+//         "subCategory": [
+//           "music",
+//           "video",
+//           "video-streaming",
+//           "music-streaming",
+//           "live-events",
+//           "nft-marketplaces"
+//         ]
+//       },
+//       {
+//         "category": "defi",
+//         "subCategory": [
+//           "banking",
+//           "personal-finance",
+//           "exchanges",
+//           "insurance",
+//           "on-ramping",
+//           "off-ramping",
+//           "payments",
+//           "finance",
+//           "airdrop tool",
+//           "others",
+//           "tooling",
+//           "prediction markets",
+//           "lending-and-borrowing",
+//           "infrastructure"
+//         ]
+//       },
+//       {
+//         "category": "food and drink",
+//         "subCategory": [
+//           "cooking",
+//           "recipes",
+//           "restaurant-finding"
+//         ]
+//       },
+//       {
+//         "category": "games",
+//         "subCategory": [
+//           "action",
+//           "adventure",
+//           "puzzle",
+//           "role-playing",
+//           "strategy",
+//           "racing",
+//           "board ",
+//           "simulation",
+//           "word",
+//           "metaverse"
+//         ]
+//       },
+//       {
+//         "category": "health and fitness",
+//         "subCategory": [
+//           "workout-apps",
+//           "meditation",
+//           "nutrition",
+//           "sleep-trackers",
+//           "medical"
+//         ]
+//       },
+//       {
+//         "category": "lifestyle",
+//         "subCategory": [
+//           "home-automation",
+//           "fashion",
+//           "dating"
+//         ]
+//       },
+//       {
+//         "category": "kids",
+//         "subCategory": []
+//       },
+//       {
+//         "category": "news",
+//         "subCategory": [
+//           "sports-news",
+//           "magazines",
+//           "decentralized-news-platforms",
+//           "newspapers",
+//           "live-news"
+//         ]
+//       },
+//       {
+//         "category": "photography",
+//         "subCategory": [
+//           "photo-editing",
+//           "camera-apps",
+//           "photo-sharing"
+//         ]
+//       },
+//       {
+//         "category": "productivity",
+//         "subCategory": [
+//           "note-taking",
+//           "task-management",
+//           "time-management",
+//           "calendar",
+//           "decentralized-collaboration-tools",
+//           "graphics and design"
+//         ]
+//       },
+//       {
+//         "category": "shopping",
+//         "subCategory": [
+//           "ecommerce",
+//           "nft-marketplaces",
+//           "price aggregator"
+//         ]
+//       },
+//       {
+//         "category": "social networking",
+//         "subCategory": [
+//           "decentralized-social-networks",
+//           "messaging"
+//         ]
+//       },
+//       {
+//         "category": "sports",
+//         "subCategory": [
+//           "team-management",
+//           "live-scores"
+//         ]
+//       },
+//       {
+//         "category": "travel",
+//         "subCategory": [
+//           "navigation",
+//           "accommodation-booking",
+//           "transportation",
+//           "trip-planning"
+//         ]
+//       },
+//       {
+//         "category": "utilities",
+//         "subCategory": [
+//           "file-management",
+//           "browsers",
+//           "security-and-privacy",
+//           "wallets",
+//           "weather"
+//         ]
+//       },
+//       {
+//         "category": "nft",
+//         "subCategory": [
+//           "art",
+//           "pfps",
+//           "domain names"
+//         ]
+//       },
+//       {
+//         "category": "gambling",
+//         "subCategory": []
+//       },
+//       {
+//         "category": "social",
+//         "subCategory": [
+//           "social media"
+//         ]
+//       },
+//       {
+//         "category": "personalization",
+//         "subCategory": [
+//           "themes",
+//           "wallpapers",
+//           "customization-tools"
+//         ]
+//       }
+//     ]
+//   }`;
 
 var polygonMapping = `{
 	"Tooling": "developer tools",
@@ -275,20 +275,20 @@ var polygonMapping = `{
 
   "Education" : "education"
 }`;
-var merokuAPIData = JSON.parse(merokuJsonString).data;
+// var merokuAPIData = JSON.parse(merokuJsonString).data;
 var polygonMappingData = JSON.parse(polygonMapping);
-var merokuCategoryList: string[] = [];
+// var merokuCategoryList: string[] = [];
 var polygonMappedList: string[] = [];
 
-merokuAPIData.map((e) => merokuCategoryList.push(e.category));
+// merokuAPIData.map((e) => merokuCategoryList.push(e.category));
 
 (Object.values(polygonMappingData) as string[]).map((e) => {
   polygonMappedList.push(e.split(".")[0]);
 });
 
-var othersList: string[] = merokuCategoryList.filter(
-  (value) => !polygonMappedList.includes(value.toLowerCase())
-);
+// var othersList: string[] = merokuCategoryList.filter(
+//   (value) => !polygonMappedList.includes(value.toLowerCase())
+// );
 
 interface CatSubCat {
   category: string;
@@ -324,5 +324,19 @@ const customToMerokuCategory = (
   }
   return output;
 };
+const getOthersCategoryList = (
+  merokuCategoryAPIData: Array<{
+    category: string,
+    subCategory: Array<string>
+  }>
+): Array<string> => {
+  var merokuCategoryList: string[] = [];
+  merokuCategoryAPIData.map((e) => merokuCategoryList.push(e.category));
+  var othersList: string[] = merokuCategoryList.filter(
+    (value) => !polygonMappedList.includes(value.toLowerCase())
+  );
+  return othersList;
+};
+
 
 export { polygonMappedList, customToMerokuCategory };
