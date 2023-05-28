b1d41dedd0d4f63c680cc63487b83c267b2a56f5
Abhimanyu121 <abhimanyushekhawat17.as@gmail.com>
2023-05-28 19:53:21 +0530

    Fixed up popup size and behaviour

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -29,32 +29,32 @@ function NavBar(props) {
     }
     return (
         <Row center
-             className="py-4 px-10 border-b border-b-[#141217] bg-canvas-color px-4 py-2 md:py-4 md:px-10 gap-[16px]">
+            className="py-4 px-10 border-b border-b-[#141217] bg-canvas-color px-4 py-2 md:py-4 md:px-10 gap-[16px]">
             <div className="flex-initial">
                 <NavItem href="/" className="pr-[20px]">
                     <NXTImage width={App.logo.width} height={App.logo.height} src={App.logo.src}
-                              alt={`${App.name} Logo`}/>
+                        alt={`${App.name} Logo`} />
                 </NavItem>
             </div>
             <div className="flex-grow flex gap-[16px] text-[14px] leading-[20px] font-[500] text-[#87868C]">
                 <button className={isActive(posConfig)}
-                        onClick={() => onAppConfigClick(posConfig)}>{posConfig.title}</button>
+                    onClick={() => onAppConfigClick(posConfig)}>{posConfig.title}</button>
                 <button className={isActive(zkevmConfig)}
-                        onClick={() => onAppConfigClick(zkevmConfig)}>{zkevmConfig.title}</button>
+                    onClick={() => onAppConfigClick(zkevmConfig)}>{zkevmConfig.title}</button>
             </div>
-            <ConnectButton chainStatus="none" showBalance={false}/>
+            <ConnectButton chainStatus="none" showBalance={false} />
         </Row>
     )
 }
 
 function NavItem(props) {
     return <Link href={props.href}
-                 className={"flex-initial py-4 text-[14px] text-[#67666E] hover:text-[#fff] font-[600]" + props.className}>{props.children}</Link>
+        className={"flex-initial py-4 text-[14px] text-[#67666E] hover:text-[#fff] font-[600]" + props.className}>{props.children}</Link>
 }
 
 
 function ExpansionPanel(props) {
-    const {open, onClick, category} = props;
+    const { open, onClick, category } = props;
     // const [isExpanded, setExpanded] = useState<boolean>(open);
     const isExpanded = open;
     const hasSubCategories = props.category.subCategory.length > 0;
@@ -66,20 +66,20 @@ function ExpansionPanel(props) {
                     <p className="text-[20px] py-[10px] capitalize">{category.category}</p>
                 </Link>
                 {hasSubCategories &&
-                  <div className={isExpanded ? "rotate-180" : ""}>
-                    <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M6 9.5L12 15.5L18 9.5" stroke="#E2E1E6" strokeWidth="2" strokeLinecap="round"
-                            strokeLinejoin="round"/>
-                    </svg>
-                  </div>}
+                    <div className={isExpanded ? "rotate-180" : ""}>
+                        <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M6 9.5L12 15.5L18 9.5" stroke="#E2E1E6" strokeWidth="2" strokeLinecap="round"
+                                strokeLinejoin="round" />
+                        </svg>
+                    </div>}
             </div>
 
             {open && hasSubCategories && category.subCategory.map((e) =>
-                (<div className="pl-5">
-                    <Link href={`/categories/?categories=${category.category}&subCategory=${e}`}>
-                        <p className="text-[16px] text-[#87868C] font-[500] py-[10px] hover:text-[#fff] capitalize">{e}</p>
-                    </Link>
-                </div>)
+            (<div className="pl-5">
+                <Link href={`/categories/?categories=${category.category}&subCategory=${e}`}>
+                    <p className="text-[16px] text-[#87868C] font-[500] py-[10px] hover:text-[#fff] capitalize">{e}</p>
+                </Link>
+            </div>)
             )
             }
         </div>
@@ -108,9 +108,9 @@ function CategoryList(props) {
     return (
         <ul>
             {data.data.map((e) => <ExpansionPanel open={openKey === e.category}
-                                                  onClick={(value) => openKey === e.category ? setOpenKey(''): setOpenKey(value)}
-                                                  category={e}
-                                                  key={e.category}/>)}
+                onClick={(value) => openKey === e.category ? setOpenKey('') : setOpenKey(value)}
+                category={e}
+                key={e.category} />)}
         </ul>
     )
 
@@ -123,7 +123,7 @@ function Input(props) {
 
     useEffect(() => {
         if (value) {
-            router.push(`/search?search=${value}`, undefined, {shallow: true});
+            router.push(`/search?search=${value}`, undefined, { shallow: true });
             //  document.getElementById("searchBar")?.focus({ preventScroll: true});
         }
     }, [value])
@@ -135,23 +135,23 @@ function Input(props) {
                     <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path
                             d="M15.0259 13.8475L18.595 17.4158L17.4159 18.595L13.8475 15.0258C12.5198 16.0902 10.8684 16.6691 9.16669 16.6667C5.02669 16.6667 1.66669 13.3067 1.66669 9.16666C1.66669 5.02666 5.02669 1.66666 9.16669 1.66666C13.3067 1.66666 16.6667 5.02666 16.6667 9.16666C16.6691 10.8683 16.0902 12.5198 15.0259 13.8475ZM13.3542 13.2292C14.4118 12.1416 15.0024 10.6837 15 9.16666C15 5.94333 12.3892 3.33333 9.16669 3.33333C5.94335 3.33333 3.33335 5.94333 3.33335 9.16666C3.33335 12.3892 5.94335 15 9.16669 15C10.6837 15.0024 12.1416 14.4118 13.2292 13.3542L13.3542 13.2292Z"
-                            fill="white"/>
+                            fill="white" />
                     </svg>
                 </div>
                 <input value={value} id='searchBar'
-                       onChange={(evt) => {
-                           setValue(evt.target.value);
-                       }}
-                       className="w-full p-2 pl-[48px] bg-canvas-color border border-border-color rounded-lg"
-                       type="search"
-                       placeholder={AppStrings.searchDapps}/>
+                    onChange={(evt) => {
+                        setValue(evt.target.value);
+                    }}
+                    className="w-full p-2 pl-[48px] bg-canvas-color border border-border-color rounded-lg"
+                    type="search"
+                    placeholder={AppStrings.searchDapps} />
             </div>
         </div>
     )
 }
 
 export function Hero(props) {
-    const {title, subtitle, video, button} = props;
+    const { title, subtitle, video, button } = props;
 
     return (
         <>
@@ -166,15 +166,15 @@ export function Hero(props) {
                         </div>
                         <div className="flex-initial sm:w-1/2 md:flex-grow">
                             <iframe src={`${video}?transparent=0&background=1&controls=1&autoplay=1&loop=1`}
-                                    className="lg:w-[40vw] h-[50vh]"
-                                    frameBorder="0"
-                                    allow="autoplay; fullscreen"
-                                    allowFullScreen/>
+                                className="lg:w-[40vw] h-[50vh]"
+                                frameBorder="0"
+                                allow="autoplay; fullscreen"
+                                allowFullScreen />
                         </div>
                     </Row>
                 </div>
                 <div
-                    className="absolute inset-0 bg-gradient-to-b from-transparent to-transparent z-0 pointer-events-none"/>
+                    className="absolute inset-0 bg-gradient-to-b from-transparent to-transparent z-0 pointer-events-none" />
             </div>
         </>
     )
@@ -183,56 +183,60 @@ export function Hero(props) {
 function CategoryListSmall(props) {
     const router = useRouter();
     const data = getPolygonCategoryList();
+    const currentCategory = (router.query.categories as string | undefined);
+    const [openKey, setOpenKey] = useState<string>(currentCategory ?? '');
 
-    const [openKey, setOpenKey] = useState<string>((router.query.categories as string | undefined) || '');
     const [selected, setSelected] = useState<string>((router.query.subCategory as string | undefined) || '')
 
     return (
         <Row className="lg:hidden overflow-scroll gap-[16px] py-[32px]">
             {data.data.map((e, index) => (
-                <details key={JSON.stringify(e)} onToggle={() => {setOpenKey(e.category)}}>
+                <details key={JSON.stringify(e)} onToggle={() => { setOpenKey(e.category) }}>
                     <summary
                         className="cursor-pointer bg-[#212026] rounded-[32px] flex justify-between items-center py-[8px] px-[12px]">
                         <Link href={`/categories/?categories=${e.category}`} >
-                        <div
-                            className="capitalize whitespace-nowrap text-[14px] leading-[21px]">{e.subCategory.includes(selected) ? selected : e.category}</div></Link>
-                            {
-                                (e.subCategory.length > 0) ?
+                            <div
+                                className="capitalize whitespace-nowrap text-[14px] leading-[21px]">{e.subCategory.includes(selected) ? selected : e.category}</div></Link>
+                        {
+                            (e.subCategory.length > 0) ?
+                                (e.subCategory.includes(selected)) ?
+
+                                    <svg className="ml-[16px]" width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg" onClick={() => {
+                                        router.push(`/categories/?categories=${data.data[0].category}`, undefined, { shallow: true })
+                                        setSelected('');
+                                        setOpenKey('')
+                                    }}>
+                                        <path d="M1 1L11 11M11 1L1 11" stroke="#E2E1E6" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                    </svg>
+                                    :
                                     (openKey === e.category) ?
                                         (
-                                        <svg className="ml-[16px]" width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M6 9.5L12 15.5L18 9.5" stroke="#E2E1E6" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                                        </svg>
-                                        ) : 
-                                            (e.subCategory.includes(selected)) ?
-                                            
-                                                <svg className="ml-[16px]"  width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg" onClick={()=>{
-                                                    router.push(`/categories/?categories=${e.category}`,undefined,{shallow: true})
-                                                    setSelected('');}}>
-                                            <path d="M1 1L11 11M11 1L1 11" stroke="#E2E1E6" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                            <svg className="ml-[16px]" width="18" height="18" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M6 9.5L12 15.5L18 9.5" stroke="#E2E1E6" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                                             </svg>
-                                            : <svg className="ml-[16px]" width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M6 9.5L12 15.5L18 9.5" stroke="#E2E1E6" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                        ) :
+                                        <svg className="ml-[16px]" width="18" height="18" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M6 9.5L12 15.5L18 9.5" stroke="#E2E1E6" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                                         </svg>
-                                    : <></>
-                            }
+                                : <></>
+                        }
                     </summary>
                     {openKey === e.category && e.subCategory.length > 0 &&
-                      <div className="cursor-pointer mt-[16px] inset-0">
-                       <Card> 
-                         {
-                         e.subCategory.map((f) => 
-                         {
-                            return <p key={JSON.stringify(e)} onClick={() => {
-                            router.push(`/categories/?categories=${e.category}&subCategory=${f.toString()}`, undefined, { shallow: true });
-                            setSelected(f);
-                            }} className="capitalize whitespace-nowrap py-[12px] text-[14px] leading-[21px]">
-                                {f}
-                            </p>
-})
-                        }
-                        </Card>
-                      </div>}
+                        <div className="cursor-pointer mt-[16px] inset-0">
+                            <Card>
+                                {
+                                    e.subCategory.map((f) => {
+                                        return <p key={JSON.stringify(e)} onClick={() => {
+                                            router.push(`/categories/?categories=${e.category}&subCategory=${f.toString()}`, undefined, { shallow: true });
+                                            setSelected(f);
+                                            setOpenKey('');
+                                        }} className="capitalize whitespace-nowrap py-[12px] text-[14px] leading-[21px]">
+                                            {f}
+                                        </p>
+                                    })
+                                }
+                            </Card>
+                        </div>}
                 </details>
             ))
             }
@@ -254,15 +258,15 @@ export function PageLayout(props) {
     let child;
     if (isLoading || isFetching) {
         child = (<div className="mr-[16px]">
-            <div className="shimmer w-full h-[48px] mb-[16px] rounded-lg"/>
-            <div className="shimmer w-full h-[48px] mb-[16px] rounded-lg"/>
-            <div className="shimmer w-full h-[48px] mb-[16px] rounded-lg"/>
-            <div className="shimmer w-full h-[48px] mb-[16px] rounded-lg"/>
+            <div className="shimmer w-full h-[48px] mb-[16px] rounded-lg" />
+            <div className="shimmer w-full h-[48px] mb-[16px] rounded-lg" />
+            <div className="shimmer w-full h-[48px] mb-[16px] rounded-lg" />
+            <div className="shimmer w-full h-[48px] mb-[16px] rounded-lg" />
         </div>)
     }
     // else if (!data.data) child= <h1>Error</h1>
     else {
-        child = <CategoryList/>;
+        child = <CategoryList />;
     }
     return (
         <article className="container">
@@ -273,23 +277,23 @@ export function PageLayout(props) {
                         className="text-[20px] leading-[27px] lg:text-[42px] lg:leading-[48px] font-[500]">{app.title}</span>
                 </div>
                 <div className="flex-initial w-full md:w-3/12">
-                    <Input/>
+                    <Input />
                 </div>
             </Row>
-            
-            {data && <CategoryListSmall data={data}/>}
-            
+
+            {data && <CategoryListSmall data={data} />}
+
             <Row className="items-start justify-start">
                 <aside className="hidden lg:flex md:flex-initial w-3/12 border-r border-r-border-color">
                     <div className="w-full">
                         <div className="py-4 border-b border-b-border-color">
                             <Link href="/history">
                                 <svg className="inline-block mr-4" width="24" height="25" viewBox="0 0 24 25"
-                                     fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path
                                         d="M22 12.5C22 18.0228 17.5228 22.5 12 22.5M22 12.5C22 6.97715 17.5228 2.5 12 2.5M22 12.5H2M12 22.5C6.47715 22.5 2 18.0228 2 12.5M12 22.5C14.5013 19.7616 15.9228 16.208 16 12.5C15.9228 8.79203 14.5013 5.23835 12 2.5M12 22.5C9.49872 19.7616 8.07725 16.208 8 12.5C8.07725 8.79203 9.49872 5.23835 12 2.5M2 12.5C2 6.97715 6.47715 2.5 12 2.5"
                                         stroke="#E2E1E6" stroke-width="2" stroke-linecap="round"
-                                        stroke-linejoin="round"/>
+                                        stroke-linejoin="round" />
                                 </svg>
                                 <span className="text-xl">{AppStrings.browsingHistory}</span>
                             </Link>
@@ -317,16 +321,16 @@ export default function Layout(props) {
         <>
             <div {...props}>
                 <div className="fixed h-[70px] w-full z-20">
-                    <NavBar/>
+                    <NavBar />
                 </div>
                 <main className="relative top-[70px]">
                     {router.asPath === '/' && <div className="min-h-[80vh]">
-                      <Hero
-                        title={app.hero.title}
-                        subtitle={app.hero.title}
-                        button={app.hero.button}
-                        video={app.hero.video}
-                      />
+                        <Hero
+                            title={app.hero.title}
+                            subtitle={app.hero.title}
+                            button={app.hero.button}
+                            video={app.hero.video}
+                        />
                     </div>}
                     {props.children}
                 </main>
