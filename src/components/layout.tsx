c387fa06648bd36bbc77df692620dbae4f3584d0
Abhimanyu121 <abhimanyushekhawat17.as@gmail.com>
2023-06-04 15:05:40 +0530

    fixed opening of dapps from featued section

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,15 +2,15 @@ import { ConnectButton } from '@rainbow-me/rainbowkit';
 import { default as NXTImage } from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import React, {ReactComponentElement, ReactElement, useEffect, useRef, useState} from "react";
+import React, { ReactComponentElement, ReactElement, useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { App, posConfig, zkevmConfig } from "../app/constants.js";
 import { getApp, setApp } from "../features/app/app_slice";
-import {getPolygonCategoryList, useGetCategoryListQuery, useGetFeaturedDappsQuery} from "../features/dapp/dapp_api";
+import { getPolygonCategoryList, useGetCategoryListQuery, useGetFeaturedDappsQuery } from "../features/dapp/dapp_api";
 import { AppStrings } from "../pages/constants";
 import { Button, Card } from "./index";
 import { Row } from "./layout/flex";
-import {FeaturedCard, SliderButton} from "./card";
+import { FeaturedCard, SliderButton } from "./card";
 import Slider from "react-slick";
 
 
@@ -319,7 +319,7 @@ export function PageLayout(props) {
 export default function Layout(props) {
     const app = useSelector(getApp);
     const router = useRouter();
-    const {data, isLoading} = useGetFeaturedDappsQuery();
+    const { data, isLoading } = useGetFeaturedDappsQuery();
     const slider = useRef();
     const settings = {
         dots: false,
@@ -361,36 +361,36 @@ export default function Layout(props) {
                 </div>
                 <main className="relative top-[70px]">
                     {router.asPath === '/' && <section>
-                      <div className="min-h-[80vh]">
-                        <Hero
-                            title={app.hero.title}
-                            subtitle={app.hero.title}
-                            button={app.hero.button}
-                            video={app.hero.video}
-                        />
-                      </div>
-                         <div className="container relative">
+                        <div className="min-h-[80vh]">
+                            <Hero
+                                title={app.hero.title}
+                                subtitle={app.hero.title}
+                                button={app.hero.button}
+                                video={app.hero.video}
+                            />
+                        </div>
+                        <div className="container relative">
                             <Row className="justify-between items-center my-[32px]">
-                              <h2 className="text-[24px] leading-[32px] lg:text-[60px] lg:leading-[72px] font-[500]">{AppStrings.featuredDapps}</h2>
-                              <div>
+                                <h2 className="text-[24px] leading-[32px] lg:text-[60px] lg:leading-[72px] font-[500]">{AppStrings.featuredDapps}</h2>
+                                <div>
                                     <SliderButton onClick={slider?.current?.slickPrev}><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                      <path d="M19 12L5 12M5 12L12 19M5 12L12 5" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                                        <path d="M19 12L5 12M5 12L12 19M5 12L12 5" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                                     </svg>
                                     </SliderButton>
                                     <SliderButton onClick={slider?.current?.slickNext}><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                      <path d="M5 12H19M19 12L12 5M19 12L12 19" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                                        <path d="M5 12H19M19 12L12 5M19 12L12 19" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                                     </svg>
                                     </SliderButton>
-                              </div>
+                                </div>
                             </Row>
                             <Slider ref={slider} {...settings}>
                                 {data ? data.map((dapp) =>
-                                    <Link href={`/dapp?id=${dapp.id}`}><FeaturedCard app={dapp}/></Link>)
-                              : buildLoadingCard(5)}
+                                    <Link href={`/dapp?id=${dapp.dappId}`}><FeaturedCard app={dapp} /></Link>)
+                                    : buildLoadingCard(5)}
                             </Slider>
                         </div>
                     </section>
-                      }
+                    }
                     {props.children}
                 </main>
             </div>
