e03992d54e465eeab2cc62de6490b010a6d48c9e
Akshit Ostwal <akshitostwal@gmail.com>
2023-06-05 18:40:26 +0530

    fixed cursor on hover

--- src/components/app_list.tsx
+++ src/components/app_list.tsx
@@ -1,11 +1,11 @@
-import { Dapp } from "../features/dapp/models/dapp";
-import { Card, RImage as Image, Text } from "./index";
-import Link from "next/link";
-import { Row } from "./layout/flex";
-import { ReactNode } from "react";
 import classNames from "classnames";
-import { spaceMono } from "../theme";
+import Link from "next/link";
 import { useRouter } from "next/router";
+import { ReactNode } from "react";
+import { Dapp } from "../features/dapp/models/dapp";
+import { spaceMono } from "../theme";
+import { Card, RImage as Image, Text } from "./index";
+import { Row } from "./layout/flex";
 
 export function Tag(props: { children: ReactNode }) {
     const _classNames = classNames({
@@ -23,7 +23,7 @@ export function AppList(props) {
 
     return (
         <div className="grid gap-8 grid-cols-1 md:grid-cols-2 3xl:grid-cols-3">
-            {props.data.length ? props.data.map((app: Dapp) => <div className="cursor-pointer" key={app.dappId} onClick={() => { router.push(`/dapp?id=${app.dappId}`) }}> <Card key={app.dappId}>
+            {props.data.length ? props.data.map((app: Dapp) => <Link href={`/dapp?id=${app.dappId}`}> <Card key={app.dappId}>
                 {/* <Link href={}> */}
                 <Row className="justify-between">
                     <Image src={app.images.logo} width={64} height={64} className="rounded-lg" alt="" />
@@ -34,7 +34,7 @@ export function AppList(props) {
                 <p className="text-[24px] leading-[32px] font-[500] my-4">{app.name}</p>
                 <Text classNames='text-[16px] leading-[21px] text-[#87868C] font-[400] line-clamp-3' maxLines={3}>{app.description}</Text>
                 {/* </Link> */}
-            </Card></div>) : <p className="text-xl">Oh No! We didnt find any dApps</p>}
+            </Card></Link>) : <p className="text-xl">Oh No! We didnt find any dApps</p>}
         </div>
     )
 }
