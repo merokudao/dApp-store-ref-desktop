cf8c448ce77b704454db31a84e94f0bcd56b33f5
Akshit Ostwal <akshitostwal@gmail.com>
2023-06-07 15:06:29 +0530

    updated readme and added contact us + FAQ

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,34 +1,34 @@
 export const App = {
-  name: "Web Store",
-  logo: {
-    height: 42,
-    width: 115,
-    src: "/slick_logo_transparent.png",
-  },
-  menu: [
-    // {
-    //     'href': '/',
-    //     title: 'dApps',
-    // },
-    // {
-    //     'href': '/categories',
-    //     title: 'Categories',
-    // },
-  ],
+	name: "Web Store",
+	logo: {
+		height: 42,
+		width: 115,
+		src: "/slick_logo_transparent.png",
+	},
+	menu: [
+		// {
+		//     'href': '/',
+		//     title: 'dApps',
+		// },
+		// {
+		//     'href': '/categories',
+		//     title: 'Categories',
+		// },
+	],
 };
 
 interface AppConfig {
-  title: string;
-  chainId: number;
-  hero: {
-    title: string;
-    subtitle: string;
-    button: {
-      text: string;
-      href: string;
-    };
-    video: string;
-  };
+	title: string;
+	chainId: number;
+	hero: {
+		title: string;
+		subtitle: string;
+		button: {
+			text: string;
+			href: string;
+		};
+		video: string;
+	};
 }
 
 // const zkevmConfig:AppConfig = {
@@ -46,18 +46,18 @@ interface AppConfig {
 // }
 
 const posConfig: AppConfig = {
-  title: "dApps",
-  chainId: 137,
-  hero: {
-    title: "There are more dApps built #OnPolygon than ever before",
-    subtitle:
-      "The dApps in our ecosystem set the standard for privacy,security and content quality.",
-    button: {
-      text: "Submit your dApp",
-      href: "",
-    },
-    video: "https://player.vimeo.com/video/791153898?h=da72488da5",
-  },
+	title: "dApps",
+	chainId: 137,
+	hero: {
+		title: "There are more dApps built #OnPolygon than ever before",
+		subtitle:
+			"The dApps in our ecosystem set the standard for privacy,security and content quality.",
+		button: {
+			text: "Submit your dApp",
+			href: "",
+		},
+		video: "https://player.vimeo.com/video/791153898?h=da72488da5",
+	},
 };
 
 export type { AppConfig };
--- src/pages/constants.tsx
+++ src/pages/constants.tsx
@@ -1,23 +1,23 @@
 const AppStrings = {
-    title: 'Polygon dApps',
-    back: 'Back',
-    allDapps: 'All dApps',
-    visitDapp: 'Visit Dapp',
-    searchDapps: 'Search dApps',
-    browsingHistory: 'Browsing History',
-    connectWallet: 'Connect Wallet',
-    about: 'About',
-    gallery: 'Gallery',
-    social: 'Social',
-    readMore: 'Read More',
-    readLess: 'Read Less',
-    featuredDapps: 'Featured dApps',
-    reviewsTitle: 'Ratings & Reviews',
-    ratings: 'Ratings'
-}
+	title: "Polygon dApps",
+	back: "Back",
+	allDapps: "All dApps",
+	visitDapp: "Visit Dapp",
+	searchDapps: "Search dApps",
+	browsingHistory: "Browsing History",
+	connectWallet: "Connect Wallet",
+	about: "About",
+	gallery: "Gallery",
+	social: "Social",
+	readMore: "Read More",
+	readLess: "Read Less",
+	featuredDapps: "Featured dApps",
+	reviewsTitle: "Ratings & Reviews",
+	ratings: "Ratings",
+	contactUs: "Contact Us",
+	faq: "FAQ",
+};
 
-export {
-    AppStrings
-}
-const defaultExport = () => { }
+export { AppStrings };
+const defaultExport = () => {};
 export default defaultExport;
