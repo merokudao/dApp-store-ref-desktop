d77e3878ff97077a6ed30464abff5f37cd0ce7d1
Akshit Ostwal <akshitostwal@gmail.com>
2023-06-08 12:57:22 +0530

    testing 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+<<<<<<< HEAD
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
 		extend: {
@@ -35,4 +36,42 @@ module.exports = {
 		},
 	},
 	plugins: [],
+=======
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  optimizeFonts: false,
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)"],
+        mono: ["var(--font-mono)"],
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "1rem",
+          lg: "1rem",
+          xl: "5rem",
+          "2xl": "6rem",
+        },
+      },
+      backgroundImage: {
+        "card-bg": "linear-gradient(180deg, #141217 0%, #0E0C12 100%);",
+      },
+      borderRadius: {
+        "card-radius": "12px",
+      },
+      colors: {
+        "text-color": "#ffffff",
+        "canvas-color": "#0a090d",
+        "border-color": "#2D2C33",
+      },
+      fontSize: {
+        "4xl": ["2rem", "120%"],
+        "5xl": ["2.625rem", "114%"],
+      },
+    },
+  },
+  plugins: [],
+>>>>>>> 47e6910 (testing 2)
 };
