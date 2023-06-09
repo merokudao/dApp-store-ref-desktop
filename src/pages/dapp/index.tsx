44f9b94d9f7758ab71fafe04154c6f0c5cc52b49
Akshit Ostwal <akshitostwal@gmail.com>
2023-06-09 11:32:36 +0530

    Fix visit dapp alignment on mobile

--- src/pages/dapp/index.tsx
+++ src/pages/dapp/index.tsx
@@ -386,9 +386,9 @@ function DappList(props) {
                             </div>
                         </div>
                         <div className="flex-initial flex">
-                            <Button as="a" className="flex flex-grow" target="_blank"
+                            <Button as="a" className="flex flex-grow justify-center" target="_blank"
                                 href={viewLink}>
-                                <div className="text-[12px] leading-[16px] lg:text-[14px] font-[500]">{AppStrings.visitDapp}</div>
+                                <div className="text-[12px] mx-2 lg:text-[14px] font-[500]">{AppStrings.visitDapp}</div>
                                 <svg className="mx-2 w-[16px] hover:text-black" width="24" height="24" viewBox="0 0 24 24" fill="none"
                                     xmlns="http://www.w3.org/2000/svg">
                                     <path d="M7 17L17 7M17 7V17M17 7H7" stroke="currentColor" strokeWidth="2" strokeLinecap="round"
