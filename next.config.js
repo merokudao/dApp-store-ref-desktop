bbd6db408d450e8a5e5b32967624c68a631a5587
thebitparticle <sanjit.maddamsetty@bitpack.me>
2023-06-13 15:25:53 +0530

    add: redirects OS metadata links to explorer pages

--- next.config.js
+++ next.config.js
@@ -26,6 +26,16 @@ const nextConfig = {
 	},
 	reactStrictMode: false,
 
+	async redirects() {
+		return [
+			{
+				source: "/app/:id",
+				destination: "/dapp?id=:id",
+				permanent: true,
+			},
+		];
+	},
+
 	webpack: (config) => {
 		config.resolve.fallback = { fs: false, net: false, tls: false };
 		return config;
