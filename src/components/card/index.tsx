6b5d641d37b2418a21a53df1c2010dc0d3be3118
abhimanyu121 <abhimanyushekhawat17.as@gmail.com>
2023-06-14 15:01:41 +0530

    fixed card outline

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -5,7 +5,7 @@ import { StarRating } from "../../pages/dapp";
 
 export const Card = (props) => {
     return (
-        <div className={"card p-4 w-full h-full bg-card-bg border border-gray-700 rounded-card-radius " + props.className}>
+        <div className="card p-4 w-full h-full bg-card-bg border border-[#ffffff1a] rounded-card-radius">
             {props.children}
         </div>
     )
