bfd26ed22b740a32f93c4b8a1cfd50a239b8c1de
abhimanyu121 <abhimanyushekhawat17.as@gmail.com>
2023-06-18 10:39:18 +0530

    fixed featured section

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -12,6 +12,9 @@ import { getPolygonCategoryList, useGetCategoryListQuery, useGetFeaturedDappsQue
 import { AppStrings } from "../pages/constants";
 import { Button, Card } from "./index";
 import { Row } from "./layout/flex";
+import { FeaturedCard, SliderButton } from "./card";
+import Slider from "react-slick";
+
 
 
 function NavBar(props) {
@@ -443,7 +446,81 @@ export default function Layout(props) {
                             video={app.hero.video}
                         />
                     </div>
+                        <div className="container relative">
+                            <Row className="justify-between items-center my-[32px]">
+                                <h2 className="text-[24px] leading-[32px] lg:text-[60px] lg:leading-[72px] font-[500]">
+                                    {AppStrings.featuredDapps}
+                                </h2>
+                                <div>
+                                    <SliderButton
+                                        onClick={() => {
+                                            (
+                                                slider?.current as any
+                                            ).slickPrev();
+                                        }}
+                                    >
+                                        <svg
+                                            width="24"
+                                            height="24"
+                                            viewBox="0 0 24 24"
+                                            fill="none"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                        >
+                                            <path
+                                                d="M19 12L5 12M5 12L12 19M5 12L12 5"
+                                                stroke="white"
+                                                strokeWidth="2"
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                            />
+                                        </svg>
+                                    </SliderButton>
+                                    <SliderButton
+                                        onClick={() => {
+                                            (
+                                                slider?.current as any
+                                            ).slickNext();
+                                        }}
+                                    >
+                                        <svg
+                                            width="24"
+                                            height="24"
+                                            viewBox="0 0 24 24"
+                                            fill="none"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                        >
+                                            <path
+                                                d="M5 12H19M19 12L12 5M19 12L12 19"
+                                                stroke="white"
+                                                strokeWidth="2"
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                            />
+                                        </svg>
+                                    </SliderButton>
+                                </div>
+                            </Row>
+                            <Slider ref={slider} {...settings}>
+                                {data
+                                    ? data.map((dapp) => (
+                                        <Link
+                                            key={app.dappId}
+                                            href={`/dapp?id=${dapp.dappId}`}
+                                            draggable={false}
+                                            onClick={(e) =>
+                                                dragging &&
+                                                e.preventDefault()
+                                            }
+                                        >
+                                            <FeaturedCard app={dapp} />
+                                        </Link>
+                                    ))
+                                    : buildLoadingCard(5)}
+                            </Slider>
+                        </div>
+
                     </div>}
+
 
                     <div className={` ${router.pathname === '/' ? `pt-[70px]` : `pt-[45px]`}`} id="allDappsScroll" />
 
