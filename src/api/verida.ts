87eacf9bfa9eceda1a44121e1abe2a51ef5a72ad
Chris <chris@verida.io>
2023-07-10 19:02:26 +0200

    Add basic attribution click tracking that stores in the Verida database controlled by the dappstore owner

---
+++ src/api/verida.ts
@@ -0,0 +1,58 @@
+import { Client, Context } from '@verida/client-ts'
+import { AutoAccount } from '@verida/account-node'
+import { EnvironmentType, IContext, Web3CallType } from '@verida/types'
+
+const VERIDA_ENVIRONMENT = EnvironmentType.TESTNET
+const CONTEXT_NAME = 'PolygonID: dAppstore'
+const CLICK_DB_NAME = 'clicks'
+
+export interface Click {
+    wallet: string,
+    url?: string,
+    insertedAt?: string
+}
+
+const DID_CLIENT_CONFIG = {
+    callType: <Web3CallType> 'web3',
+    web3Config: {
+        rpcUrl: 'https://polygon-mumbai.g.alchemy.com/v2/Q4NRuRlwTNyI90dDCgiX_KT_vS_2gpbN',
+        privateKey: ''
+    }
+}
+
+let veridaContext = <IContext | undefined> undefined
+
+export const getVeridaContext = async function(): Promise<IContext | undefined> {
+    if (veridaContext) {
+        return veridaContext
+    }
+
+    // establish a network connection
+    const client = new Client({
+        environment: VERIDA_ENVIRONMENT
+    })
+
+    // create a Verida account instance that wraps the authorized Verida DID server connection
+    // The `AutoAccount` instance will automatically sign any consent messages
+    const account = new AutoAccount({
+        privateKey: '',
+        environment: VERIDA_ENVIRONMENT,
+        didClientConfig: DID_CLIENT_CONFIG
+    })
+
+    // Connect the Verida account to the Verida client
+    await client.connect(account)
+
+    // Open an application context (forcing creation of a new context if it doesn't already exist)
+    const context = await client.openContext(CONTEXT_NAME, true)
+    if (context) {
+        veridaContext = context
+    }
+
+    return veridaContext
+}
+
+export const getClickDb = async function() {
+    const context = await getVeridaContext()
+    return await context!.openDatabase(CLICK_DB_NAME)
+}
---
+++ src/pages/api/click.ts
@@ -0,0 +1,16 @@
+import { getClickDb, Click } from "../../api/verida";
+
+export default async function click(req, res) {
+    if (req.method == 'POST') {
+        const click = <Click> {
+            wallet: req.body.wallet,
+            url: req.body.url
+        }
+
+        // @todo: validate data
+
+        const database = await getClickDb()
+        await database.save(click)
+        res.status(200).json({ success: true })
+    }
+}
